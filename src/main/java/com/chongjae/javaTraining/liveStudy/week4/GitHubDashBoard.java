961bc5b67f9bd98c4f12100e5cf98579315e9fdb
조홍제 <bjq913@naver.com>
2020-12-03 22:52:59 +0900

    GitHubDashBoard class 구현

--- src/main/java/com/chongjae/javaTraining/liveStudy/week4/GitHubDashBoard.java
+++ src/main/java/com/chongjae/javaTraining/liveStudy/week4/GitHubDashBoard.java
@@ -3,7 +3,11 @@ package com.chongjae.javaTraining.liveStudy.week4;
 import org.kohsuke.github.*;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class GitHubDashBoard {
     private final GitHub gitHub;
@@ -12,19 +16,36 @@ public class GitHubDashBoard {
         this.gitHub = new GitHubBuilder().withOAuthToken(OAuthToken).build();
     }
 
-    public List<GHIssue> getIssues(String ownerAndRepository, GHIssueState issueState) throws IOException {
-        return gitHub.getRepository(ownerAndRepository).getIssues(issueState);
+    public GHRepository getRepository(String ownerAndRepository) throws IOException {
+        return gitHub.getRepository(ownerAndRepository);
     }
 
-    public List<GHIssueComment> getComments(GHIssue ghIssue) throws IOException {
-        return ghIssue.getComments();
+    public List<GHIssue> getIssues(GHRepository repository, GHIssueState issueState) throws IOException {
+        return repository.getIssues(issueState);
     }
 
-    public String getUserLoginIdByComment(GHIssueComment ghIssueComment) throws IOException {
-        return ghIssueComment.getUser().getLogin();
+    public Set<String> getUsersWhoWroteCommentsInIssue(GHIssue ghIssue) throws IOException {
+        return ghIssue.getComments()
+                .stream()
+                .map(ghIssueComment -> {
+                    try {
+                        return ghIssueComment.getUser().getLogin();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                })
+                .collect(Collectors.toSet());
     }
 
-    public void calculateParticipationRate() {
+    public void removeNullInSet(Set<String> set) {
+        set.removeAll(Collections.singleton(null));
+    }
 
+    public void calculateParticipationRate(Map<String, Integer> participants, int totalStudyNumber) {
+        for (Map.Entry<String, Integer> entry : participants.entrySet()) {
+            System.out.println("참여자 : " + entry.getKey() +
+                    " 참석율 : " + String.format("%.2f", (double) entry.getValue() / (double) totalStudyNumber * 100) + "%");
+        }
     }
 }
