b9f1636c6e85b53f2447427bd1d8641671dbbc5a
조홍제 <bjq913@naver.com>
2020-12-07 23:52:32 +0900

    LinkedList 구현 및 테스트 코드 추가

--- src/main/java/com/chongjae/javaTraining/liveStudy/week4/LinkedList.java
+++ src/main/java/com/chongjae/javaTraining/liveStudy/week4/LinkedList.java
@@ -1,33 +1,68 @@
 package com.chongjae.javaTraining.liveStudy.week4;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class LinkedList {
-    private final List<ListNode> listNodeList = new ArrayList<>();
-    private ListNode headListNode;
-    private ListNode lastListNode;
+    int size = 1;
 
-    public List<ListNode> getNodeList() {
-        return listNodeList;
+    ListNode add(ListNode head, ListNode nodeToAdd, int position) throws IllegalAccessException {
+        if (position <= 0 || position > size) {
+            throw new IllegalAccessException("잘못된 position입니다.");
+        } else {
+            ListNode node = head;
+            for (int i = 0; i < position - 1; i++) {
+                node = node.getNextListNode();
+            }
+            ListNode previousNextNode = node.getNextListNode();
+            node.setNextListNode(nodeToAdd);
+            nodeToAdd.setNextListNode(previousNextNode);
+            size++;
+            return nodeToAdd;
+        }
     }
 
-    public ListNode getLastNode() {
-        return lastListNode;
+    ListNode remove(ListNode head, int positionToRemove) throws IllegalAccessException {
+        if (positionToRemove <= 0 || positionToRemove >= size) {
+            throw new IllegalAccessException("잘못된 position입니다.");
+        }
+        ListNode node = head;
+        for (int i = 0; i < positionToRemove - 1; i++) {
+            node = node.getNextListNode();
+        }
+
+        ListNode previousNextNode = node.getNextListNode().getNextListNode();
+        node.setNextListNode(previousNextNode);
+        size--;
+        return previousNextNode;
     }
 
-    public ListNode add(ListNode head, ListNode nodeToAdd, int position) {
-
+    boolean contains(ListNode head, ListNode nodeToCheck) {
+        ListNode node = head;
+        while (node != null) {
+            if (node == nodeToCheck) {
+                return true;
+            }
+            node = node.getNextListNode();
+        }
+        return false;
     }
 
-    public ListNode remove(ListNode listNode) {
+    public static void main(String[] args) throws IllegalAccessException {
+        ListNode head = new ListNode(1);
+        LinkedList linkedList = new LinkedList();
 
-    }
+        ListNode add = linkedList.add(head, new ListNode(2), 1);
+        ListNode add1 = linkedList.add(head, new ListNode(3), 2);
+        ListNode add2 = linkedList.add(head, new ListNode(4), 3);
+        ListNode add3 = linkedList.add(head, new ListNode(5), 4);
+        System.out.println(add);
+        ListNode add4 = linkedList.add(head, new ListNode(6), 2);
+        System.out.println(add4);
 
-    public boolean contains(ListNode listNode) {
+        System.out.println(linkedList.contains(head, new ListNode(3)));
 
-    }
+        ListNode remove = linkedList.remove(head, 2);
+        System.out.println(remove);
+        ListNode remove1 = linkedList.remove(head, 4);
+        System.out.println(remove1);
 
-    public static void main(String[] args) {
     }
 }
--- src/test/groovy/com/chongjae/javaTraining/liveStudy/week4/LinkedListTest.groovy
+++ src/test/groovy/com/chongjae/javaTraining/liveStudy/week4/LinkedListTest.groovy
@@ -3,14 +3,49 @@ package com.chongjae.javaTraining.liveStudy.week4
 import spock.lang.Specification
 
 class LinkedListTest extends Specification {
-    def "Test"() {
+    def "add 테스트"() {
+        given:
+        ListNode head = new ListNode(1)
+        LinkedList linkedList = new LinkedList();
         when:
-        1 == 1
-//        LinkedList linkedList = new LinkedList();
-//        linkedList.add(1)
-//        def result = linkedList.getNodeList()
+        def node1 = linkedList.add(head, new ListNode(2), 1)
         then:
-        true
-//        result != null
+        head.getNextListNode() == node1
+        and:
+        when:
+        def node2 = linkedList.add(head, new ListNode(3), 1)
+        then:
+        head.getNextListNode() == node2
+        and:
+        when:
+        def node3 = linkedList.add(head, new ListNode(4), 2)
+        then:
+        head.getNextListNode().getNextListNode() == node3
+    }
+
+    def "remove 테스트"() {
+        given:
+        ListNode head = new ListNode(1)
+        LinkedList linkedList = new LinkedList();
+        when:
+        def node1 = linkedList.add(head, new ListNode(2), 1)
+        def node2 = linkedList.add(head, new ListNode(3), 2)
+        linkedList.remove(head, 1)
+        then:
+        head.getNextListNode() == node2
+    }
+
+    def "contains 테스트"() {
+        given:
+        ListNode head = new ListNode(1)
+        LinkedList linkedList = new LinkedList();
+        when:
+        def node1 = linkedList.add(head, new ListNode(2), 1)
+        def node2 = linkedList.add(head, new ListNode(3), 2)
+        def result1 = linkedList.contains(head, node2)
+        def result2 = linkedList.contains(head, new ListNode(4))
+        then:
+        result1
+        !result2
     }
 }
